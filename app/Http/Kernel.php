93da7f352f3deb15d54489938e8ae43410c7d05f
BanyarThaw <banyarthaw2@gmail.com>
2023-05-11 00:26:32 +0630

    auth middleware adding

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkUser' => \App\Http\Middleware\checkUser::class,
+        'admin' => \App\Http\Middleware\admin::class,
     ];
 }
